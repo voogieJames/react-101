de9d2feecf3f28d22306907e02a76c6dd4737ebe
Vugar Abdulzada <vugar@origin.no>
2019-01-17 13:28:16 +0100

    Preparing exercise 1

--- src/ContactsList.test.js
+++ src/ContactsList.test.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD:src/App.test.js
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-=======
 import ContactsList from './ContactsList';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<ContactsList />, div);
->>>>>>> 2959e33894587296ed9941faa8c0dccaf137bfe9:src/ContactsList.test.js
   ReactDOM.unmountComponentAtNode(div);
 });
